af0231d68191873f517f064bea59f15fab00ffc0
Sparkandflash <phegde04@gmail.com>
2021-10-14 15:40:16 +0530

    fixed mediaquery hook

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { React, Window, useState, useEffect } from 'react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import Link from 'next/link';
 import {
@@ -14,113 +14,144 @@ const s = "15px";
 function Header() {
     const [isMinWidth, setIsMinWidth] = useState(false);
     const [mediaQuery] = useMediaQuery("(min-width: 910px)");
-  console.log(mediaQuery);
+    console.log(mediaQuery);
 
-    useEffect(() => { 
+    useEffect(() => {
         setIsMinWidth(mediaQuery);
     }, []);
-        return (
+    return (
 
-            <header>
-                <Box h="90px" w="100%" p={2}>
-                   
-                    {isMinWidth ?
-                        <Center>
-                            <Flex>
-                                <Box p="2">
-                                    <Text fontSize={s}>
-                                        <Link href="/">RGV 1.0.0A</Link>
-                                    </Text>
-                                </Box>
-                                <Spacer />
-                                <Box p="2">
-                                    <Text fontSize={s}>
-                                        <Link href="/privatetesting">Rug Monitor </Link>
-                                    </Text>
-                                </Box>
-                                <Spacer />
-                                <Box p="2">
-                                    <Text fontSize={s}>
-                                        <Link href="/walletscanner">Wallet Scanner </Link>
-                                    </Text>
-                                </Box>
-                                <Spacer />
-                                <Box p="2">
-                                    <Text fontSize={s}>
-                                        <Link href="/sniper">Sniper</Link>
-                                    </Text>
-                                </Box>
-                                <Spacer />
-                                <Box p="2">
-                                    <Text fontSize={s}>
-                                        <Link href="/privatetesting">List of Known Scammers</Link>
-                                    </Text>
-                                </Box>
-                                <Spacer />
-                                <Box p="2">
-                                    <Text fontSize={s}>
-                                        <Link href="/privatetesting">Tornado Cash Tracer</Link>
-                                    </Text>
-                                </Box>
-                                <Spacer />
-                                <Box p="2">
-                                    <Text fontSize={s}>
-                                        <Link href="/privatetesting"> Typhoon Finance Tracer</Link>
-                                    </Text>
-                                </Box>
-                            </Flex>
-                        </Center>
+        <header>
+            <Box h="90px" w="100%" p={2}>
 
-                        :
+                {isMinWidth ?
+                    <Center>
+                        <Flex>
+                            <Box p="2">
+                                <Text fontSize={s}>
+                                    <Link href="/">RGV 1.0.0A</Link>
+                                </Text>
+                            </Box>
+                            <Spacer />
+                            <Box p="2">
+                                <Text fontSize={s}>
+                                    <Link href="/privatetesting">Rug Monitor </Link>
+                                </Text>
+                            </Box>
+                            <Spacer />
+                            <Box p="2">
+                                <Text fontSize={s}>
+                                    <Link href="/walletscanner">Wallet Scanner </Link>
+                                </Text>
+                            </Box>
+                            <Spacer />
+                            <Box p="2">
+                                <Text fontSize={s}>
+                                    <Link href="/sniper">Sniper</Link>
+                                </Text>
+                            </Box>
+                            <Spacer />
+                            <Box p="2">
+                                <Text fontSize={s}>
+                                    <Link href="/privatetesting">List of Known Scammers</Link>
+                                </Text>
+                            </Box>
+                            <Spacer />
+                            <Box p="2">
+                                <Text fontSize={s}>
+                                    <Link href="/privatetesting">Tornado Cash Tracer</Link>
+                                </Text>
+                            </Box>
+                            <Spacer />
+                            <Box p="2">
+                                <Text fontSize={s}>
+                                    <Link href="/privatetesting"> Typhoon Finance Tracer</Link>
+                                </Text>
+                            </Box>
+                        </Flex>
+                    </Center>
 
-                        <Box p={3}>
-                            <Menu>
-                                <MenuButton
-                                    size="lg"
-                                    as={IconButton}
-                                    outlineColor="lightblue"
-                                    aria-label="Options"
-                                    icon={<HamburgerIcon />}
+                    :
 
-                                />
-                                <MenuList
-
-                                >
-                                    <VStack>
-                                        <MenuItem >
-                                            <Link href="/">RGV 1.0.0A</Link>
-                                        </MenuItem>
-                                        <MenuDivider />
-                                        <MenuItem >
-                                            <Link href="/privatetesting">Rug Monitor </Link>
-                                        </MenuItem>
-                                        <MenuItem >
-                                            <Link href="/walletscanner">Wallet Scanner </Link>
-                                        </MenuItem>
-                                        <MenuItem >
-                                            <Link href="/sniper">Sniper</Link>
-                                        </MenuItem>
-                                        <MenuItem >
-                                            <Link href="/privatetesting">List of Known Scammers</Link>
-                                        </MenuItem>
-                                        <MenuItem >
-                                            <Link href="/privatetesting">Tornado Cash Tracer</Link>
-                                        </MenuItem>
-                                        <MenuItem >
-                                            <Link href="/privatetesting"> Typhoon Finance Tracer</Link>
-                                        </MenuItem>
-                                    </VStack>
-                                </MenuList>
-                            </Menu>
-                        </Box>
-
-                    }
+                    <Box p={3}>
+                        <Menu
+                            closeOnBlur="false"
+                        >
+                            <MenuButton
+                                size="lg"
+                                as={IconButton}
+                                colorScheme="cyan"
+                                aria-label="Options"
+                                icon={<HamburgerIcon />}
 
 
+                            />
+                            <MenuList
+
+                            >
+                                <VStack>
+                                    <MenuItem >
+                                        <Box p="1">
+                                            <Text fontSize="md">
+                                                <Link href="/">RGV 1.0.0A</Link>
+                                            </Text>
+                                        </Box>
+                                    </MenuItem>
+                                    <MenuDivider />
+                                    <MenuItem >
+                                        <Box p="1">
+                                            <Text fontSize={s}>
+                                                <Link href="/privatetesting">Rug Monitor </Link>
+                                            </Text>
+                                        </Box>
+                                    </MenuItem>
+                                    <MenuItem >
+                                        <Box p="1">
+                                            <Text fontSize={s}>
+                                                <Link href="/walletscanner">Wallet Scanner </Link>
+                                            </Text>
+                                        </Box>
+                                    </MenuItem>
+                                    <MenuItem >
+                                        <Box p="1">
+                                            <Text fontSize={s}>
+                                                <Link href="/sniper">Sniper</Link>
+                                            </Text>
+                                        </Box>
+                                    </MenuItem>
+                                    <MenuItem >
+                                        <Box p="1">
+                                            <Text fontSize={s}>
+                                                <Link href="/privatetesting">List of Known Scammers</Link>
+                                            </Text>
+                                        </Box>
+                                    </MenuItem>
+                                    <MenuItem >
+                                        <Box p="1">
+                                            <Text fontSize={s}>
+                                                <Link href="/privatetesting">Tornado Cash Tracer</Link>
+                                            </Text>
+                                        </Box>
+                                    </MenuItem>
+                                    <MenuItem >
+                                        <Box p="1">
+                                            <Text fontSize={s}>
+                                                <Link href="/privatetesting"> Typhoon Finance Tracer</Link>
+                                            </Text>
+                                        </Box>
+                                    </MenuItem>
+                                </VStack>
+                            </MenuList>
+                        </Menu>
+                    </Box>
+
+                }
 
 
-                </Box>
-            </header>
-        )
+
+
+            </Box>
+        </header>
+    )
 }
 export default Header
